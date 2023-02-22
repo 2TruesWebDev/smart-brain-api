024adc1a548b8bf424096ef4e7ac98cad0417d96
2TruesWebDev <2trueswebdev@gmail.com>
2023-02-22 16:24:34 -0600

    new changes

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE' 
+ apiKey: 'd11c247a254f48909edbace8fb8dd951' 
 });
 
 

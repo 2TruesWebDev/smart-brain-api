00042a1c62363a4f16210dc388f5fece74df2a12
2TruesWebDev <2trueswebdev@gmail.com>
2023-02-22 18:49:05 -0600

    update server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : '35.160.120.126',
     user : 'postgres',
     password : '2trues',
     database : 'smart-brain'
